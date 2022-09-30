0b41e22d06e4d96e9b1fb1ecf40d8bd523993632
unknown <badin-016@badin-016.ad.badin.rs>
2022-09-30 10:51:16 +0200

    3rd hour of tutorial

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const _ = require('lodash');
-const items = [1,[2],[3],[4]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end('Home page')
+    }else if(req.url === '/about'){
+        // BLOCKING CODE!!!
+        for(let i = 0; i<=100;i++){
+            for(let j = 0; j<=1000;j++){
+                console.log(`i: ${i}, j: ${j}`);
+            }
+        }
+        res.end('About Page');
+    }else{
+        res.end('Error page');
+    }
+
+
+});
+
+server.listen(5000,()=>{
+    console.log('Server Listening on port 5000 ...');
+});
